3c8e30be89c2fe80c8f52d9e9acade88f1021fdb
Geunseong Jung <dreamwayjgs@gmail.com>
2021-05-25 15:05:29 +0900

    live update

--- Chap2_Python+Web-Flask/project/live/app.py
+++ Chap2_Python+Web-Flask/project/live/app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import json
+
+from flask.json import dump
 
 
 app = Flask(__name__)
@@ -6,7 +9,30 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return ""
+    return render_template("index.html")
+
+
+@app.route("/greet")
+def greet():
+    return json.dumps({"hello": "world"})
+
+
+@app.route("/order", methods=["POST"])
+def order():
+    print(request.get_json())
+    print(request)
+    data = {"greet": "hello", "timestamp": "오늘"}
+    print(type(data), data)
+    dumped = json.dumps(data)
+    print(type(dumped), dumped)
+    loaded = json.loads(dumped)
+    print(type(loaded), loaded)
+    return "OK"
+
+
+@app.route("/order", methods=["PUT"])
+def xxx():
+    return "OK"
 
 
 if __name__ == "__main__":
